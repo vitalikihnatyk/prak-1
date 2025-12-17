0e25f94c28fd5cd8efc19cfa21a409baa5df788b
vitalikihnatyk <zon.03safd@gmail.com>
2025-12-17 10:44:00 +0200

    sdfg

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cout << "Введіть два числа: ";
+    std::cin >> a >> b;
+
+    std::cout << "Сума: " << a + b << std::endl;
+    return 0;
+}
